8805c448712ddd9e4b1148ff412325784bded8cd
Sabina B <sabina.motrescu@gmail.com>
2023-01-10 19:28:45 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,10 @@ public class Main {
 
         int product1 = 24;
         int product2 = 35;
-        int product3 = 125;
-        int product4 = 111;
-        int product5 = 21;
-        int product6 = 364;
+        int product3 = 12;
+        int product4 = 6;
+        int product5 = 8;
+        int product6 = 3;
 
         int multiplicationresult;
         multiplicationresult = product1 * product2 * product3 * product4 * product5 * product6;
